4b55ba52f0d7efcf2d42f6fe21678d9fc0377232
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-01-07 18:23:41 +1100

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Giuseppe Sapienza on 7/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
